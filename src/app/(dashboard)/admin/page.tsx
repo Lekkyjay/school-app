173ffac66ff81fe754690e19f907f25189b987c4
Lekky Jay <lekkyjay@yahoo.com>
2024-10-17 20:23:40 +0200

    Implemented Dashboard components

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,8 @@
+import Announcements from '@/components/Announcements'
+import AttendanceChart from '@/components/AttendanceChart'
+import CountChart from '@/components/CountChart'
+import EventCalendar from '@/components/EventCalendar'
+import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 
 export default function Admin() {
@@ -16,27 +21,22 @@ export default function Admin() {
         <div className="flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
-            {/* <CountChart /> */}
-            CountChart goes here
+            <CountChart />
           </div>
           {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            {/* <AttendanceChart /> */}
-            AttendanceChart goes here
+            <AttendanceChart />
           </div>
         </div>
         {/* BOTTOM CHART */}
         <div className="w-full h-[500px]">
-          {/* <FinanceChart /> */}
-          FinanceChart goes here
+          <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        {/* <EventCalendar />
-        <Announcements/> */}
-        EventCalendar goes here
-        Announcements goes here
+        <EventCalendar />
+        <Announcements/>
       </div>
     </div>
   )
