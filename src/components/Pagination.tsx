76a4280f8e4f338f17efb4b0c8343ca73c639831
Lekky Jay <lekkyjay@yahoo.com>
2024-10-20 11:15:17 +0200

    Refactored

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,7 +10,7 @@ export default function Pagination() {
         Prev
       </button>
       <div className="flex items-center gap-2 text-sm">
-        <button className="px-2 rounded-sm bg-lamaSky">1</button>
+        <button className="px-2 rounded-sm bg-lekkySky">1</button>
         <button className="px-2 rounded-sm ">2</button>
         <button className="px-2 rounded-sm ">3</button>
         ...
